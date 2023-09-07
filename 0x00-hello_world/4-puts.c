4508beffeb335defaa5e15790830e5612d06c280
Kislame <shadoooo198@gmail.com>
2023-09-07 14:09:22 -0700

    fixed some stuff

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message using the `puts` function
+ *              and returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 
-int main() {
     puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
