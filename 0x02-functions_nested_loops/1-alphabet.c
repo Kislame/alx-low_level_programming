6fd346560a99c5e2046042eba85639328af89889
Kislame <shadoooo198@gmail.com>
2023-09-12 10:42:27 -0700

    aa

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *print_alphabet function
- *using putchar twice
+ *print_alphabet function implmentation using putchar
+ * 
  *followed by a new line
  */
 void print_alphabet(void)
