ea8e11943cdbd684bacb7db3a389b61eb04010c7
Kislame <shadoooo198@gmail.com>
2023-09-12 13:55:21 -0700

    added

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,12 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	n = _abs(n);
-
 	last_digit = n % 10;
+
+	if (last_digit < 0)
+	{
+		last_digit = -last_digit;
+	}
 
 	_putchar('0' + last_digit);
 
