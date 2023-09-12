a62e1f7912eca2ae141af10b8a37b96cf5780213
Kislame <shadoooo198@gmail.com>
2023-09-12 14:02:23 -0700

    dk u betty

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- *print_last_digit - Prints and returns the last digit. 
+ *print_last_digit - Prints and returns the last digit
  *@n: The number to extract the last digit from.
- *
  *Return: The value of the last digit.
  */
 int print_last_digit(int n)
