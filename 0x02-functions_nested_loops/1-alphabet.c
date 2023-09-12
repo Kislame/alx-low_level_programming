6b75b564ecedff31d069c7d6b00b79e39998de4c
Kislame <shadoooo198@gmail.com>
2023-09-12 10:00:33 -0700

    fixed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ *print_alphabet - Prints the alphab
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar('\n');
+}
+
+/**
  *main - Entry point
  *
  *Return: Always 0 (Success)
